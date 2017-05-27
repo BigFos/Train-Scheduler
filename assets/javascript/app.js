becdcf2bb09ef3fde00bc10b8cc2feda53ce467c
Steven Foster Jr <Steven Foster Jr>
2017-05-27 00:34:35 -0700

    updated code to HTML and JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,6 @@
     var database = firebase.database();
     var trainName = "";
     var dest = "";
-    var firstTrain = "";
     var freq = 0;
     
     var minAway = "" ;
@@ -18,9 +17,8 @@
         event.preventDefault();
         trainName = $("#trainName").val().trim();
         dest = $("#dest").val().trim();
-        firstTrain = $("#firstTrain").val().trim();
         freq = $("#freq").val().trim();
-        var nextArrival = moment(firstTrain, "hmm").format("hh:mm A");
+        
         database.ref().push({
             trainName: trainName,
             dest: dest,
@@ -28,18 +26,52 @@
             freq: freq,
             
         })
-        $("#tName").html(trainName);
-        $("#destination").html(dest);
-        $("#frequency").html(freq);
-        $("#nextArrival").html(nextArrival);
-        $("#minutesAway").html(moment(minAway).diff(moment(), "months") * -1);
-        console.log(minAway);
-        console.log(nextArrival);
-        console.log(firstTrain);
+        // $("#tName").html(trainName);
+        // $("#destination").html(dest);
+        // $("#frequency").html(freq);
+        // $("#nextArrival").html(nextArrival);
+        // $("#minutesAway").html( moment(nextTrain).format("hh:mm A"));
+        // console.log(minAway);
+        // console.log(nextArrival);
+        // console.log(firstTrain);
        
     });
     // database.ref().on("child_added",function(snapshot){
     //   $("#info").append("<td>" + trainName +  "</td><td>" + dest + "</td> <td>" + firstTrain +  "</td><td>" + freq + "</td><td>" + nextArrival + "</td><td>" + minAway +  "</td>");
     // });
-    // $("#submit").on("click", function {
-    // })
+    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+  // Store everything into a variable.
+  // var firstTrainTime = $("#firstTrain").val().trim();
+  var trainName = childSnapshot.val().trainName;
+  var dest = childSnapshot.val().dest;
+  var firstTrain = childSnapshot.val().firstTrain;
+  var freq = childSnapshot.val().freq;
+  // Employee Info
+  console.log(trainName);
+  console.log(dest);
+  console.log(firstTrain);
+  console.log(freq);
+ var firstTrConv = moment(firstTrain, "hmm").format("hh:mm A");
+ console.log(firstTrConv);
+        var firstTiConv = moment(firstTrConv, "hh:mm").subtract(1, "years");
+        console.log(firstTiConv);
+        // Current Time
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+        // Difference between the times
+        var diffTime = moment().diff(moment(firstTiConv), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+        // Time apart (remainder)
+        var timeRemain = diffTime % freq;
+        console.log(timeRemain);
+        // Minute Until Train
+        var nextArrival = freq - timeRemain;
+        console.log("MINUTES TILL TRAIN: " + nextArrival);
+        // Next Train
+        var nextTrain = moment().add(nextArrival, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm A"));
+  // Add each train's data into the table
+  $("#employee-table > tbody").append("<tr><td>" + trainName + "</td><td>" + dest + "</td><td>" +
+  freq + "</td><td>" + nextTrain + "</td><td>" + nextArrival + "</td></tr>");
+});
